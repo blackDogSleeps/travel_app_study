2da34965b09e5915aa123995a09516059e1a036b
Vasiliy Kuryshev <basilkooryshev@gmail.com>
2023-11-05 11:41:16 +0300

    added smooth scroll on navigation

--- travel-app/src/router/index.js
+++ travel-app/src/router/index.js
@@ -96,7 +96,11 @@ const router = new VueRouter({
       }
       
       if (!to.hash) {
-        return { x: 0, y: 0 };
+        return { 
+          x: 0, 
+          y: 0,
+          behavior: 'smooth',
+        };
       }
     }
   },
